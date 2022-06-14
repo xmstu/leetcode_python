d83c6ea55276fcdd5c164efd1f95e373c94451a8
hexm <hexm@huitouche.com>
2022-06-14 15:22:37 +0800

    ftr: 更新题解

--- questions/297_serialize_and_deserialize_binary_tree.py
+++ questions/297_serialize_and_deserialize_binary_tree.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from collections import deque
 from typing import List
 
 
@@ -50,6 +51,55 @@ class Codec:
         root.left = self.restore()
         root.right = self.restore()
         return root
+    
+
+class CodecBfs:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        q = deque()
+        q.appendleft(root)
+        res = ''
+        while q:
+            node = q.pop()
+            if node != None:
+                res += str(node.val) + ','
+                q.appendleft(node.left)
+                q.appendleft(node.right)
+            else:
+                res += 'null,'
+        return res
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        data = data.split(',')
+        root = TreeNode(int(data[0]))
+        q = deque()
+        q.appendleft(root)
+        cursor = 1
+        while q:
+            node = q.pop()
+            left_val, right_val = data[cursor], data[cursor + 1]
+            if left_val != 'null':
+                node.left = TreeNode(int(left_val))
+                q.appendleft(node.left)
+            if right_val != 'null':
+                node.right = TreeNode(int(right_val))
+                q.appendleft(node.right)
+            cursor += 2
+        return root
 
         
 class TestCodec:
@@ -78,3 +128,15 @@ class TestCodec:
         assert new_root.right.val == 3
         assert new_root.right.left.val == 4
         assert new_root.right.right.val == 5
+
+        codec_bfs = CodecBfs()
+
+        tree_str = codec_bfs.serialize(root)
+
+        new_root = codec_bfs.deserialize(tree_str)
+        assert new_root.val == 1
+        assert new_root.left.val == 2
+        assert new_root.right.val == 3
+        assert new_root.right.left.val == 4
+        assert new_root.right.right.val == 5
+
