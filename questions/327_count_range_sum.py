968c78dd49bb37f2162bfafac92a224d63fa6406
hexm <hexm@huitouche.com>
2022-07-03 00:56:50 +0800

    ftr: 更新题解

--- questions/327_count_range_sum.py
+++ questions/327_count_range_sum.py
@@ -3,6 +3,12 @@ from typing import List
 
 
 class Solution:
+    """
+    给你一个整数数组 nums 以及两个整数 lower 和 upper 。求数组中，值位于范围 [lower, upper] （包含 lower 和 upper）之内的 区间和的个数 。
+    区间和 S(i, j) 表示在 nums 中，位置从 i 到 j 的元素之和，包含 i 和 j (i ≤ j)。
+
+    意思是给你个lower，upper上下限，让你从目标数组arr里找各种可能的子数组满足子数组的sum求和在给定上下限之内
+    """
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         pass
 
@@ -14,4 +20,10 @@ class TestCountRangeSum:
     """
 
     def test(self):
-        pass
+        solution = Solution()
+
+        nums = [-2,5,-1]; lower = -2; upper = 2
+        assert 3 == solution.countRangeSum(nums, lower, upper)
+
+        nums = [0]; lower = 0; upper = 0
+        assert 1 == solution.countRangeSum(nums, lower, upper)
---
+++ questions/374_guess_number_higher_or_lower.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res == 1:
+                left = mid + 1
+            elif res == -1:
+                right = mid - 1
