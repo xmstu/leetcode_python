4aa5bd12a0a6d4e74797925ec3142d2b0df0279b
hexm <hexm@huitouche.com>
2022-07-02 16:36:54 +0800

    ftr: 更新130题解

--- questions/130_surrounded_regions.py
+++ questions/130_surrounded_regions.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
+from collections import deque
 from typing import List
 
-class Solution:
+class SolutionDfs:
 
     """
     给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充
@@ -32,30 +33,64 @@ class Solution:
 
         # 从边界开始dfs
         for col in range(ncol):
-            if board[0][col] == "O":
-                dfs(0, col)
-            if board[nrow - 1][col] == "O":
-                dfs(nrow - 1, col)
+            dfs(0, col)
+            dfs(nrow - 1, col)
         for row in range(nrow):
-            if board[row][0] == "O":
-                dfs(row, 0)
-            if board[row][ncol - 1] == "O":
-                dfs(row, ncol - 1)
-    
+            dfs(row, 0)
+            dfs(row, ncol - 1)
         
-        # 遍历一遍矩阵, 将所有还是 O 的全部改为 X
-        for row in range(nrow):
-            for col in range(ncol):
-                if board[row][col] == "O":
-                    board[row][col] = "X"
-        
-
-        # 遍历一遍矩阵, 将所有是 B 的全部还原为 O
+        # 遍历一遍矩阵, 将所有是 B 的还原为 O, 再将所有是 O 的改为 X
         for row in range(nrow):
             for col in range(ncol):
                 if board[row][col] == "B":
                     board[row][col] = "O"
+                elif board[row][col] == "O":
+                    board[row][col] = "X"
+        
+class SolutionBfs:
 
+    """
+    给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充
+    """
+    def solve(self, board: List[List[str]]) -> None:
+        nrow, ncol = len(board), len(board[0])
+        DIRS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        def in_area(x, y):
+            return 0 <= x < nrow and 0 <= y < ncol
+
+        q = deque()
+        # 从边界开始遍历
+        for col in range(ncol):
+            if board[0][col] == "O":
+                q.appendleft((0, col))
+            if board[nrow - 1][col] == "O":
+                q.appendleft((nrow - 1, col))
+        for row in range(nrow):
+            if board[row][0] == "O":
+                q.appendleft((row, 0))
+            if board[row][ncol - 1] == "O":
+                q.appendleft((row, ncol - 1))
+        
+        # 广度优先遍历
+        while q:
+            row, col  = q.pop()
+            board[row][col] = "B"
+            for dx, dy in DIRS:
+                nx, ny = row + dx, col + dy
+                if not in_area(nx, ny):
+                    continue
+                if board[nx][ny] in ("X", "B"):
+                    continue
+                q.appendleft((nx, ny))
+
+         # 遍历一遍矩阵, 将所有是 B 的还原为 O, 再将所有是 O 的改为 X
+        for row in range(nrow):
+            for col in range(ncol):
+                if board[row][col] == "B":
+                    board[row][col] = "O"
+                elif board[row][col] == "O":
+                    board[row][col] = "X"
+        
 
 class TestSolve:
 
@@ -65,7 +100,7 @@ class TestSolve:
 
     def test(self):
 
-        solution = Solution()
+        solution = SolutionBfs()
 
         board = [
             ["X","X","X","X"],
@@ -90,3 +125,7 @@ class TestSolve:
         board = [["X"]]
         solution.solve(board)
         assert [["X"]] == board
+
+        board = [["O","O","O"],["O","O","O"],["O","O","O"]]
+        solution.solve(board)
+        assert [["O","O","O"],["O","O","O"],["O","O","O"]] == board
