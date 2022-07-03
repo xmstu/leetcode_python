daac32d3ec7bf406d0c7f51623624ee254028678
hexm <hexm@huitouche.com>
2022-07-03 16:59:13 +0800

    ftr: 更新题解

--- questions/20_valid_parentheses.py
+++ questions/20_valid_parentheses.py
@@ -1,17 +1,18 @@
 # -*- coding:utf-8 -*-
+from queue import LifoQueue
 
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        stack = LifoQueue()
         for char in s:
             # 左的符号都塞到栈里
             if char in ("(", "[", "{"):
-                stack.append(char)
+                stack.put(char)
             else:
                 # 遇到右的符号, 先判断栈是否为空, 如果为空, 就返回 False
                 # 不为空, 就判断栈顶和右符号是否匹配, 不匹配返回false, 匹配就弹出栈顶
-                if not stack:
+                if stack.empty():
                     return False
                 if char == ")" and stack.queue[-1] != '(':
                     return False
@@ -19,9 +20,9 @@ class Solution:
                     return False
                 if char == "}" and stack.queue[-1] != '{':
                     return False
-                stack.pop()
+                stack.get()
         # 最后栈为空, 就说明所有符号都是一一对应的, 说明括号有效
-        return not stack
+        return stack.empty()
 
 
 class TestParenthesesIsValid:
