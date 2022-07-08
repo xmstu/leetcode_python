09c9dad69e98e985af02a29719153cfddb2d2093
hexm <hexm@huitouche.com>
2022-07-08 23:38:50 +0800

    ftr: 更新63题解

--- questions/63_unique_paths_2.py
+++ questions/63_unique_paths_2.py
@@ -36,7 +36,27 @@ class Solution:
             return res
 
         return countPaths(0, 0)
+    
 
+class Solution2:
+
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        f = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    f[i][j] = 0
+                elif i == 0 and j == 0:
+                    f[i][j] = 1
+                elif i == 0:
+                    f[i][j] = f[i][j - 1]
+                elif j == 0:
+                    f[i][j] = f[i - 1][j]
+                else:
+                    f[i][j] = f[i - 1][j] + f[i][j - 1]
+
+        return f[m - 1][n - 1]
 
 class TestUniquePath:
     """
@@ -44,7 +64,7 @@ class TestUniquePath:
     """
 
     def test(self):
-        solution = Solution()
+        solution = Solution2()
 
         obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
         assert 2 == solution.uniquePathsWithObstacles(obstacleGrid)
