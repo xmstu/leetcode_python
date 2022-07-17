77edde2ed71428a1eb874e74c15753447d801e1d
hexm <hexm@huitouche.com>
2022-07-17 23:45:53 +0800

    ftr: 更新题解

--- questions/45_jump_game_2.py
+++ questions/45_jump_game_2.py
@@ -32,13 +32,28 @@ class Solution:
         return ans
 
 
+class Solution2:
+
+
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        dp = [0] * n
+        for i in range(1, n):
+            j = 0
+            while j + nums[j] < i:
+                j += 1
+            dp[i] = dp[j] + 1
+        return dp[n-1]
+
+
 class TestJump:
     """
     pytest -s 45_jump_game_2.py::TestJump
     """
 
     def test(self):
-        solution = Solution()
+        solution = Solution2()
 
         nums = [2,3,1,1,4]
         assert 2 == solution.jump(nums)
