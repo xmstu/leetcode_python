a12894b0a5ddf3611511ca4c63a939f9bd8dd830
hexm <xinmao.he@aminer.cn>
2022-07-22 18:16:30 +0800

    ftr: 更新题解

---
+++ questions/212_word_search_2.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+
+
+class Solution:
+    """
+    给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words， 返回所有二维网格上的单词 。
+    单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+    """
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        pass
+
+
+class TestFindWords:
+
+    """
+    pytest -s 212_word_search_2.py::TestFindWords
+    """
+
+    def test(self):
+        pass
+
