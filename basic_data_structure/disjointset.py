f6b8e716753b8a96899806bcfb39862970e3d3d9
hexm <hexm@huitouche.com>
2022-07-24 17:03:10 +0800

    ftr: 更新题解

--- basic_data_structure/disjointset.py
+++ basic_data_structure/disjointset.py
@@ -2,24 +2,29 @@
 
 
 class DisjointSet:
-	"""
-	并查集
+    """
+    并查集
 
-	find 将 x 和 x 的所有祖先连在根节点上, 因此树的高度只有2层
-	unionSet 把两棵树进行合并, 只有当两个节点的最高层级的祖先不一致时, 才合并
-	"""
+    find 将 x 和 x 的所有祖先连在根节点上, 因此树的高度只有2层
+    unionSet 把两棵树进行合并, 只有当两个节点的最高层级的祖先不一致时, 才合并
+    """
 
-	def __init__(self, n) -> None:
-		self.fa = [i for i in range(n)]
-	
-	def find(self, x):
-		if x == self.fa[x]:
-			return x
-		self.fa[x] = self.find(self.fa[x])
-		return self.fa[x]
-	
-	def unionSet(self, x, y):
-		x = self.find(x)
-		y = self.find(y)
-		if x != y:
-			self.fa[x] = y
+    def __init__(self, n) -> None:
+        self.fa = [i for i in range(n)]
+    
+    def find(self, x):
+        if x != self.fa[x]:
+            self.fa[x] = self.find(self.fa[x])
+        return self.fa[x]
+    
+    def unionSetV1(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x != y:
+            self.fa[x] = y
+    
+    def unionSet(self, x, y):
+        self.fa[self.find(x)] = self.find(y)
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
--- questions/684_redundant_connection.py
+++ questions/684_redundant_connection.py
@@ -54,22 +54,15 @@ class DisjointSet:
         self.fa = [i for i in range(n)]
     
     def find(self, x):
-        if x == self.fa[x]:
-            return x
-        self.fa[x] = self.find(self.fa[x])
+        if x != self.fa[x]:
+            self.fa[x] = self.find(self.fa[x])
         return self.fa[x]
     
     def unionSet(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        if x != y:
-            self.fa[x] = y
+        self.fa[self.find(x)] = self.find(y)
 
     def connected(self, x, y):
-        if self.find(x) == self.find(y):
-            return True
-        else:
-            return False
+        return self.find(x) == self.find(y)
 
 
 class SolutionDisjointSet:
