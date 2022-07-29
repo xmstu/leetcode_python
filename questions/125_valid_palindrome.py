260a9e8b9de584f76787df5f85e689da9d45e84e
hexm <xinmao.he@aminer.cn>
2022-07-29 11:20:17 +0800

    ftr: 更新题解

--- questions/125_valid_palindrome.py
+++ questions/125_valid_palindrome.py
@@ -26,6 +26,41 @@ class Solution:
         return res
 
 
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+
+        def isDigitOrLetter(ch: str):
+            return '0' <= ch <= '9' or 'a' <= ch <= 'z' or 'A' <= ch <= 'Z'
+        
+        def getNext(s: str, i: int):
+            while i < len(s) and not isDigitOrLetter(s[i]):
+                i += 1
+            return i
+        
+        def getPre(s: str, i: int):
+            while i > 0 and not isDigitOrLetter(s[i]):
+                i -= 1
+            return i
+
+        def equalsIgnoreCase(ch1: str, ch2: str):
+            def lower(ch: str):
+                if 'A' <= ch <= 'Z':
+                    ch = ord(ch) - ord("A") + ord("a")
+                    ch = chr(ch)
+                return ch
+            return lower(ch1) == lower(ch2)
+        
+        l = getNext(s, 0)
+        r = getPre(s, len(s) - 1)
+        while l < r:
+            if not equalsIgnoreCase(s[l], s[r]):
+                return False
+            l = getNext(s, l + 1)
+            r = getPre(s, r - 1)
+        
+        return True
+
+
 
 class TestIsPalindrome:
 
@@ -33,7 +68,7 @@ class TestIsPalindrome:
 	pytest -s 125_valid_palindrome.py::TestIsPalindrome
 	"""
 	def test(self):
-		solution = Solution()
+		solution = Solution2()
 
 		s = "A man, a plan, a canal: Panama"
 		assert True == solution.isPalindrome(s)
