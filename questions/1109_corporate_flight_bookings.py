091978e7e2f41848b3fb4cbbc1afebbc7c28bbb8
hexm <hexm@huitouche.com>
2022-08-02 00:05:18 +0800

    ftr: 更新题解

--- questions/1109_corporate_flight_bookings.py
+++ questions/1109_corporate_flight_bookings.py
@@ -3,6 +3,11 @@ from typing import List
 
 
 class Solution:
+    """
+    这里有 n 个航班, 它们分别从 1 到 n 进行编号。
+    有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 意味着在从 firsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
+    请你返回一个长度为 n 的数组 answer, 里面的元素是每个航班预定的座位总数。
+    """
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
         pass
 
